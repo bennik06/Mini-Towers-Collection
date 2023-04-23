e59bdcd8cfe24820a3de8fda15fc5842df28793c
bennik06 <126697208+bennik06@users.noreply.github.com>
2023-04-23 12:32:52 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace MiniTowersCollection;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "36.2";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "36.3";
+    public const string Version = "1.0.1";
     public const string Name = "Mini Towers Collection";
 
     public const string Description =
