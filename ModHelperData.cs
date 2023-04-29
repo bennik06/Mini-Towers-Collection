9b4e76b2b97bd8c5fdcc0c0e2d00a50a5d142505
bennik06 <126697208+bennik06@users.noreply.github.com>
2023-04-29 20:25:12 +0200

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,13 +3,14 @@ namespace MiniTowersCollection;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.3";
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Name = "Mini Towers Collection";
 
     public const string Description =
         "Mini towers included:\n\n" +
         "- Upgradeable Legend of the Night\n" +
-        "- Mr Stickman's personal tower\n\n" +
+        "- Mr Stickman's personal tower\n" +
+        "- Tobyman009's personal tower\n\n" +
         "More towers will be added soon!";
 
     public const string RepoOwner = "bennik06"; // TODO add your github username hero, also in the download url in README.md
